74a8584d8b2222142a193a28cf6cd805040801da
birosbeni <birosbeni@gmail.com>
2023-07-06 07:21:35 +0200

    Add cors

--- EventTracingBackend/Program.cs
+++ EventTracingBackend/Program.cs
@@ -44,6 +44,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+           .AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader());
+
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
