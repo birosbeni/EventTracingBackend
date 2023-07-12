b6d334b365f3f6a470fa9e4f344bd4bba2ff63e0
birosbeni <birosbeni@gmail.com>
2023-07-12 16:32:43 +0200

    Add validation

--- EventTracingBackend/Models/CreateEvent.cs
+++ EventTracingBackend/Models/CreateEvent.cs
@@ -1,10 +1,21 @@
-﻿namespace EventTracingBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventTracingBackend.Models
 {
     public class CreateEvent
     {
+        [Required(ErrorMessage = "A nevet kötelező megadni")]
+        [MinLength(3, ErrorMessage = "Minimum 3 karakternek kell lennie a névnek")]
+        [MaxLength(50, ErrorMessage = "Maximum 50 karakter hosszú lehet a név")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "A nevet kötelező megadni")]
+        [MinLength(3, ErrorMessage = "Minimum 3 karakternek kell lennie a helyszínnek")]
+        [MaxLength(50, ErrorMessage = "Maximum 50 karakter hosszú lehet a helyszín")]
         public string Location { get; set; }
+
         public string? Country { get; set; }
+
         public int? Capacity { get; set; }
     }
 }
