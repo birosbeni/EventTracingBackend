be6b8c519a53bead24f6f4bc850c5da6be38827d
birosbeni <birosbeni@gmail.com>
2023-12-30 09:30:59 +0100

    Update validator

--- WebApi/Validators/LocationValidator.cs
+++ WebApi/Validators/LocationValidator.cs
@@ -14,8 +14,8 @@ namespace EventTracingBackend.BusinessLogic.Validators
             RuleFor(location => location.Country).NotEmpty().WithMessage("Country can not be empty");
             RuleFor(location => location.PostalCode).GreaterThan(0).WithMessage("Postalcode must be greater than 0");
             RuleFor(location => location.City).NotEmpty().WithMessage("City can not be empty");
-            RuleFor(location => location.Street).NotEmpty().WithMessage("Street can not be empty");
-            RuleFor(location => location.House).GreaterThan(0).WithMessage("House must be greater than 0");
+            RuleFor(location => location.Street).NotEmpty().When(location => location.House > 0).WithMessage("Street can not be empty if house is given");
+            RuleFor(location => location.House).GreaterThan(0).When(location => !string.IsNullOrEmpty(location.Street)).WithMessage("House must be greater than 0 if street is given");
         }
     }
 }
